ac9f43b7eab2244e99ec94833d8269c2032d6f41
Alex <alexkmartinez77@gmail.com>
2018-10-13 12:16:24 -0700

    changes the greeting served

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');     //require in the express library
 const app = express();                 // express() creates an 'app' object that represents a running express app; app is used to configure our routes
 
 app.get('/', (req, res) => {           // lines 4-6 represent a route handler
-    res.send({hi: 'there post'});
+    res.send({hi: 'bye buddy'});
 });
 
 const PORT = process.env.PORT || 5000;  // heroku will dynamically assign ports just as its running applications we have to account for this by setting up var to caputure the environment variable that will be passed
