c057e94a03a32b5266cdc9b555b1072d993f0297
Alex <alexkmartinez77@gmail.com>
2018-10-18 20:11:47 -0700

    adds a logout route handler allowing the user to logout and erase the cookie, and a current_user route handler to check on the id of the user

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,16 +3,18 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy; //gives EXPR
 const mongoose = require('mongoose');
 const keys = require('../config/keys'); // since password and login info is part of the path we hide them in config/keys.js
 
-const User = mongoose.model('users');   //pulls the schema out of mongoose with just one argument, 'users'
+const User = mongoose.model('users');   //pulls the schema out of mongoose with just one argument, 'users', and creates this User 'class'
 
-passport.serializeUser((user, done) => { //we are going to define a function and pass it to serializeUser, done is a callback function
-    done(null, user.id);
+passport.serializeUser((user, done) => { //is automatically called by the google strategy once scope is retrieved and existing user/user is passed into it as user again
+    done(null, user.id);                 //passport shoves this id into a cookie in the user's browser
+                                         //the anonymous function is created by us and passed to serializeUser
+                                         //user is what was just pulled out of the database
 });
 
-passport.deserializeUser((id, done) => {
-    User.findById(id)
-        .then(user => {
-            done(null, user);
+passport.deserializeUser((id, done) => { //when the user makes follow-up requests from the browser to the server passport will take the id, automatically included by browser up another request, and find that user based on that id (cookie)
+    User.findById(id)                    //we are taking the id and then turning it back into a user model
+        .then(user => {              
+            done(null, user);            //added to request object as REQ.USER
         })
 });
 
@@ -22,7 +24,7 @@ passport.use(                                   // passport.use tells passport t
         clientSecret: keys.googleClientSecret,  // is a private key that noone should know about
         callbackURL: '/auth/google/callback'    // the url the user will be redirected to once permisisons are granted by google on their way back from google
     },
-        (accessToken, refreshToken, profile, done) => {                       // callback anonymous fat arrow function
+        (accessToken, refreshToken, profile, done) => {                       // run this google strategy callback anonymous fat arrow function once we receive the profile and email
             User.findOne({ googleId: profile.id })                            // this query returns a promise
                 .then((existingUser) => {
                     if (existingUser) {
